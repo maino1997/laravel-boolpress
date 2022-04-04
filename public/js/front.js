1a8356a7dec416486d9ed28258898d2ceb02202c
Sasha Mainardi <sashamainardi4@gmail.com>
2022-04-04 16:16:05 +0200

    config installlations

--- public/js/front.js
+++ public/js/front.js
@@ -49908,7 +49908,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Laravel\laravel-many-to-many\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Laravel\laravel-boolpress\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
